0468d12e003cc3618fb16241c6661fd3ae199da2
John Haase <haas1681@gmail.com>
2024-12-25 18:37:44 -0500

    Reapply "prep for templateFile"
    This reverts commit 3ad8fc7cd3d7fa43d455745e3ab37456624d7a85.

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,7 +2,6 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 import { FileFilter } from '../renderer/utilities/createSaveAsDialog';
-import StateManager from '../renderer/types/StateManager';
 
 export type Channels = 'ipc-example';
 
@@ -22,6 +21,10 @@ const electronHandler = {
     },
     once(channel: Channels, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+    getSettingsDir: () => {
+      const result = ipcRenderer.invoke('get-settings-dir');
+      return result;
     },
     mergePresentations: async (yamlState: string) => {
       const result = await ipcRenderer.invoke('merge-presentations', yamlState);
